eb0652f7b101894a0de8b12533786b6392487a8e
Safina <safinalifa@gmail.com>
2016-08-29 22:37:45 -0400

    Add-ons

--- FirebaseIM/AppDelegate.swift
+++ FirebaseIM/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation.AVAudioSession
 import Firebase
 
 @UIApplicationMain
@@ -18,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-       
+        try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient)     
         
          FIRApp.configure()
         return true
